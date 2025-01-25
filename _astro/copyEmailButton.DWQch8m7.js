81344f8e3e50f352df12a0d6bb0314646c2ce3cf
ryanoregan <ryan8489@gmail.com>
2025-01-26 12:32:36 +1300

    Updates

---
+++ _astro/copyEmailButton.DWQch8m7.js
@@ -0,0 +1,9 @@
+import{r as p}from"./index.BL7xzsR_.js";var l={exports:{}},r={};/**
+ * @license React
+ * react-jsx-runtime.production.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var x;function C(){if(x)return r;x=1;var i=Symbol.for("react.transitional.element"),s=Symbol.for("react.fragment");function o(d,e,t){var u=null;if(t!==void 0&&(u=""+t),e.key!==void 0&&(u=""+e.key),"key"in e){t={};for(var a in e)a!=="key"&&(t[a]=e[a])}else t=e;return e=t.ref,{$$typeof:i,type:d,key:u,ref:e!==void 0?e:null,props:t}}return r.Fragment=s,r.jsx=o,r.jsxs=o,r}var c;function m(){return c||(c=1,l.exports=C()),l.exports}var n=m();function f(){const[i,s]=p.useState(!1),o=()=>{navigator.clipboard.writeText("Ryan8489@gmail.com").then(()=>{s(!0),setTimeout(()=>s(!1),2e3)})};return n.jsxs("div",{children:[n.jsxs("button",{className:"copyEmailButton",onClick:o,children:[n.jsx("svg",{viewBox:"0 0 24 24",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:n.jsx("path",{d:"M7.5 3H14.6C16.8402 3 17.9603 3 18.816 3.43597C19.5686 3.81947 20.1805 4.43139 20.564 5.18404C21 6.03969 21 7.15979 21 9.4V16.5M6.2 21H14.3C15.4201 21 15.9802 21 16.408 20.782C16.7843 20.5903 17.0903 20.2843 17.282 19.908C17.5 19.4802 17.5 18.9201 17.5 17.8V9.7C17.5 8.57989 17.5 8.01984 17.282 7.59202C17.0903 7.21569 16.7843 6.90973 16.408 6.71799C15.9802 6.5 15.4201 6.5 14.3 6.5H6.2C5.0799 6.5 4.51984 6.5 4.09202 6.71799C3.71569 6.90973 3.40973 7.21569 3.21799 7.59202C3 8.01984 3 8.57989 3 9.7V17.8C3 18.9201 3 19.4802 3.21799 19.908C3.40973 20.2843 3.71569 20.5903 4.09202 20.782C4.51984 21 5.0799 21 6.2 21Z",stroke:"currentColor","stroke-width":"2","stroke-linecap":"round","stroke-linejoin":"round"})}),"Email"]}),i&&n.jsx("div",{className:"copied",children:"Copied!"})]})}export{f as default};
