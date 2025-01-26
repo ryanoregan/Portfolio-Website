fe56914250e2d8f805e73f1a0bdb373926d19125
ryanoregan <ryan8489@gmail.com>
2025-01-26 15:22:15 +1300

    Updated configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  base: '/Portfolio-Website/'
+
+  site: "https://ryanoregan.github.io/Portfolio-Website/",
 });
